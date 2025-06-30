35f33eb92bfc1c58494533b2833ce5d2d2d48de1
Emmanuel Ozoemena <ozoemenachukwuebuka2004@gmail.com>
2025-07-01 00:08:35 +0100

    Added a margin-top to the attribution class

--- frontendMentor2/interactive-card-details-form/script.js
+++ frontendMentor2/interactive-card-details-form/script.js
@@ -5,21 +5,24 @@ const today = new Date();
 const currentYear = today.getFullYear();
 
 function cardHolderName() {
-  const cardHolderName = document.querySelector(".js-name-input").value;
+  const inputField = document.querySelector(".js-name-input");
+  const cardHolderName = inputField.value;
   let errorMsg = document.querySelector(".js-name-error");
 
   if (cardHolderName === "") {
     errorMsg.innerHTML = "Can't be blank";
+    inputField.classList.add("error-input");
     notFilled = true;
   } else {
     errorMsg.innerHTML = "";
+    inputField.classList.remove("error-input");
   }
 }
 
 function cardNumber() {
-  const inputField = document.querySelector(".js-card-input").value;
+  const inputField = document.querySelector(".js-card-input");
   let errorMsg = document.querySelector(".js-card-error");
-  let cardNumber = inputField.replace(/\s/g, ""); // Remove existing spaces
+  let cardNumber = inputField.value.replace(/\s/g, ""); // Remove existing spaces
 
   // Remove non-digit characters
   cardNumber = cardNumber.replace(/\D/g, "");
@@ -33,60 +36,75 @@ function cardNumber() {
   // Validation
   if (cardNumber === "") {
     errorMsg.innerHTML = "Can't be blank";
+    inputField.classList.add("error-input");
     notFilled = true;
   } else if (cardNumber.length !== 16) {
     errorMsg.innerHTML = "Card number must be 16 digits";
+    inputField.classList.add("error-input");
     isInvalid = true;
   } else {
     errorMsg.innerHTML = "";
+    inputField.classList.remove("error-input");
   }
 }
 
 function expiringMonth() {
-  const expiringMonth = document.querySelector(".js-month-input").value;
+  const inputField = document.querySelector(".js-month-input");
+  const expiringMonth = inputField.value;
   let errorMsg = document.querySelector(".js-month-error");
 
   // Validation
   if (expiringMonth === "") {
     errorMsg.innerHTML = "Can't be blank";
+    inputField.classList.add("error-input");
     notFilled = true;
   } else if (expiringMonth < 1 || expiringMonth > 12) {
     errorMsg.innerHTML = "Insert a correct month";
+    inputField.classList.add("error-input");
     isInvalid = true;
   } else {
     errorMsg.innerHTML = "";
+    inputField.classList.remove("error-input");
   }
 }
 
 function expiringYear() {
-  const expiringYear = document.querySelector(".js-year-input").value;
+  const inputField = document.querySelector(".js-year-input");
+  const expiringYear = inputField.value;
   let errorMsg = document.querySelector(".js-year-error");
 
   // Validation
   if (expiringYear === "") {
     errorMsg.innerHTML = "Can't be blank";
+    inputField.classList.add("error-input");
     notFilled = true;
   } else if (expiringYear <= 25) {
     errorMsg.innerHTML = `Must be higher than ${currentYear}`;
+    inputField.classList.add("error-input");
     isInvalid = true;
   } else {
     errorMsg.innerHTML = "";
+    inputField.classList.remove("error-input");
   }
 }
 
 function cvcNumber() {
-  const cvcNumber = document.querySelector(".js-cvc-input").value;
+  const inputField = document.querySelector(".js-cvc-input");
+  const cvcNumber = inputField.value;
   let errorMsg = document.querySelector(".js-cvc-error");
 
   // Validation
   if (cvcNumber === "") {
     errorMsg.innerHTML = "Can't be blank";
+    inputField.classList.add("error-input");
     notFilled = true;
   } else if (cvcNumber.length !== 3) {
     errorMsg.innerHTML = "Must be 3 digits";
+    inputField.classList.add("error-input");
     isInvalid = true;
   } else {
     errorMsg.innerHTML = "";
+    inputField.classList.remove("error-input");
   }
 }
 
