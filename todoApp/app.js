9c559cf471c3cd12efe249ef8049bc9c31e3eb82
Emmanuel Ozoemena <ozoemenachukwuebuka2004@gmail.com>
2025-07-12 13:48:27 +0100

    Made the edit button interactive

--- todoApp/app.js
+++ todoApp/app.js
@@ -13,17 +13,17 @@ document.addEventListener("DOMContentLoaded", () => {
     todosContainer.style.width = taskList.children.length > 0 ? "100%" : "50%";
   };
 
-  const addTask = (event) => {
-    event.preventDefault();
+  const addTask = (text, completed = false) => {
+    // event.preventDefault();
 
-    const taskText = taskInput.value.trim();
+    const taskText = text || taskInput.value.trim();
     if (!taskText) {
       return;
     }
 
     const list = document.createElement("li");
     list.innerHTML = `
-      <input type="checkbox" class="checkbox" />
+      <input type="checkbox" class="checkbox" ${completed ? "checked" : ""} />
       <span>${taskText}</span>
 
       <div class="task-buttons">
@@ -39,6 +39,35 @@ document.addEventListener("DOMContentLoaded", () => {
 
     `;
 
+    const checkbox = list.querySelector(".checkbox");
+    const editBtn = list.querySelector(".edit-btn");
+
+    if (completed) {
+      list.classList.add("completed");
+      editBtn.disabled = true;
+      editBtn.style.opacity = "0.5";
+      editBtn.style.pointerEvents = "none";
+    }
+
+    checkbox.addEventListener("change", () => {
+      const isChecked = checkbox.checked;
+      list.classList.toggle("completed", isChecked);
+
+      editBtn.disabled = isChecked;
+      editBtn.style.opacity = isChecked ? "0.5" : "1";
+
+      editBtn.style.pointerEvents = isChecked ? "none" : "auto";
+    });
+
+    editBtn.addEventListener("click", () => {
+      if (!checkbox.checked) {
+        taskInput.value = list.querySelector("span").textContent;
+
+        list.remove();
+        toggleEmptyState();
+      }
+    });
+
     // Functionalities for the delete button
     list.querySelector(".delete-btn").addEventListener("click", () => {
       list.remove();
@@ -51,11 +80,13 @@ document.addEventListener("DOMContentLoaded", () => {
     toggleEmptyState();
   };
 
-  addTaskBtn.addEventListener("click", addTask);
+  addTaskBtn.addEventListener("click", () => addTask());
 
   taskInput.addEventListener("keypress", (e) => {
     if (e.key === "Enter") {
-      addTask(e);
+      e.preventDefault();
+
+      addTask();
     }
   });
 });
