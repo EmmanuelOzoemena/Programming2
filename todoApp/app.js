f3eadce7236cdc99fe7e55283eca043865926537
Emmanuel Ozoemena <ozoemenachukwuebuka2004@gmail.com>
2025-07-12 16:53:38 +0100

    Added Confetti animation for completion of tasks

--- todoApp/app.js
+++ todoApp/app.js
@@ -145,4 +145,34 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const Confetti = () => {
   // Copy and paste the Confetti animations codes from Confetti docs here
+
+  const defaults = {
+    spread: 360,
+    ticks: 50,
+    gravity: 0,
+    decay: 0.94,
+    startVelocity: 30,
+    shapes: ["star"],
+    colors: ["FFE400", "FFBD00", "E89400", "FFCA6C", "FDFFB8"],
+  };
+
+  function shoot() {
+    confetti({
+      ...defaults,
+      particleCount: 40,
+      scalar: 1.2,
+      shapes: ["star"],
+    });
+
+    confetti({
+      ...defaults,
+      particleCount: 10,
+      scalar: 0.75,
+      shapes: ["circle"],
+    });
+  }
+
+  setTimeout(shoot, 0);
+  setTimeout(shoot, 100);
+  setTimeout(shoot, 200);
 };
