93f92341fdbed7738e9c62d02a0131c0195e2015
Emmanuel Ozoemena <ozoemenachukwuebuka2004@gmail.com>
2025-07-23 16:48:23 +0100

    Added styles to the header

--- productSnap/src/components/header/Header.jsx
+++ productSnap/src/components/header/Header.jsx
@@ -4,23 +4,38 @@ const Header = () => {
   return (
     <>
       <header>
-        <div className="left-section">
-          <img src="images/logo.svg" alt="logo" />
+        <div className="container">
+          <div className="left-section">
+            <img src="images/logo.svg" alt="logo" />
+            <nav>
+              <ul>
+                <li>
+                  <a href="#">Collections</a>
+                </li>
+                <li>
+                  <a href="#">Men</a>
+                </li>
+                <li>
+                  <a href="#">Women</a>
+                </li>
+                <li>
+                  <a href="#">About</a>
+                </li>
+                <li>
+                  <a href="#">Contact</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
 
-         <nav>
-          <ul>
-            <li><a href="#">Collections</a></li>
-            <li><a href="#">Men</a></li>
-            <li><a href="#">Women</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
-          </ul>
-         </nav>
-        </div>
-
-        <div className="right-section">
-          <img src="images/icon-cart.svg" alt="cart" />
-          <img src="images/image-avatar.png" alt="avatar" />
+          <div className="right-section">
+            <img src="images/icon-cart.svg" alt="cart" />
+            <img
+              src="images/image-avatar.png"
+              alt="avatar"
+              className="avatar-pic"
+            />
+          </div>
         </div>
       </header>
     </>
