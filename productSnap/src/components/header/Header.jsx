6d9e2892091b93c8c5767c6b893f4514a06cdec6
Emmanuel Ozoemena <ozoemenachukwuebuka2004@gmail.com>
2025-07-23 20:08:13 +0100

    Reanamed container to header-container

--- productSnap/src/components/header/Header.jsx
+++ productSnap/src/components/header/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <>
       <header>
-        <div className="container">
+        <div className="header-container">
           <div className="left-section">
             {/* <div className="toggle-btn"> */}
             <img
